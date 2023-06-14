b064ea1d40089e938c73f38e0064ce2670d93597
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-06-14 16:03:05 -0700

    Added text input and button to search for movies and created an eventlistener for when that button is pressed. Also added my api key and url to show current movies.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 console.log("Running");
 
+var submitButton = document.getElementById("submitButton");
 var loadMoreMoviesButton = document.getElementById("load-more-movies-btn");
+
+var currentPage = 1;
+
+const apiKey = "6a08aabf3e9e29b260193e5aa2446497";
+const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${currentPage}`;
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    console.log("Movies Found")
+});
 
 loadMoreMoviesButton.addEventListener("click", (event) => {
     event.preventDefault();
 
     console.log("Movies Loaded Succesfully")
 });
+
+
